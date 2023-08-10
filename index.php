351241b78fba01d4d14a4d8cfbff97b2cb90eddf
GFAUnDead <contact@gfaundead.stream>
2023-08-10 11:48:57 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,46 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>YourListOnline - HOME</title>
-	<link rel="icon" href="img/logo.png" type="image/png" />
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="js/about.js"></script>
-	<style type="text/css">
-    a.popup-link {
-      text-decoration: none;
-      color: black;
-      cursor: pointer;
-    }
-  </style>
-</head>
-<body>
-	<header>
-		<h1>Welcome to Your List Online</h1>
-	</header>
-	
-	<main>
-		<p>With this website you can keep track of all the tasks you need to complete.</p>
-		<p>To get started, simply log in with your Twitch account.</p>
-		<a href="#"><button class="twitch-button">Log in with Twitch (COMING SOON)</button></a>
-		<a href="lists/dashboard.php"><button class="button">OLD SYSTEM LOGIN</button></a>
-	</main>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourListOnline - HOME</title>
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
+    <script src="https://cdn.yourlist.online/js/about.js"></script>
+  	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
+  	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
+  </head>
+  <body>
 
-	<footer>
-	<p><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
-	</footer>
-</body>
+<!-- Navigation -->
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="menu">
+      <li class="menu-text">YourListOnline</li>
+    </ul>
+  </div>
+</div>
+<!-- /Navigation -->
+
+    <div class="row column text-center">
+      <h2>Welcome to Your List Online</h2>
+      <hr>
+      <p>With this website you can keep track of all the tasks you need to complete.</p>
+		  <p>To get started, simply log in with your account or use your Twitch or Discord account.</p>
+		  <a href="https://access.yourlist.online/dashboard.php"><button class="defult-button">Login</button></a>
+		  <a href="https://twitch.yourlist.online/dashboard.php"><button class="twitch-button">Login with Twitch</button></a>
+		  <a href=""><button class="discord-button">Login with Discord (COMING SOON)</button></a>
+		  <br><br>
+		  <p><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></p>
+    </div>
+    <div class="row small-up-2 large-up-4">
+     
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
 </html>
